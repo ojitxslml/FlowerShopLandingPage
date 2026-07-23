543c4bec8720a2c6183f8603bfa9b2cf89ac9d5e
Juan Pablo Urra <jp.urra42@gmail.com>
2026-07-23 19:58:05 -0400

    add deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ojitxslml.github.io',
+  base: '/FlowerShopLandingPage',
   vite: {
     plugins: [tailwindcss()]
   }
